c59469c90b95b8b4a20ecf14c82391f34238b05a
iambatmanscape-notwork <aakashkhamaru@proton.me>
2024-11-03 05:37:32 +0000

    final touches

--- backend/main.py
+++ backend/main.py
@@ -31,8 +31,15 @@ async def get_recipes(skip:int = Query(0,ge=0),limit:int = Query(10,ge=1,le=20))
         recipe_list.append(recipe)
     return recipe_list
 
+@app.get('/recipes/getbyname/{recipe_query}',response_model=List[RecipesModel])
+async def get_recipe(recipe_query:str,skip:int = Query(0,ge=0),limit:int = Query(10,ge=1,le=20)):
+    recipe_list = []
+    cursor = recipes.find({"title": {"$regex": recipe_query, "$options": "i"}},projection={"author":1,"photo_url":1,"title":1,"rating_stars":1}).skip(skip).limit(limit)
+    async for recipe in cursor:
+        recipe_list.append(recipe)
+    return recipe_list
 
-@app.get('/recipes/{recipe_id}',response_model=RecipeModel)
+@app.get('/recipes/getbyid/{recipe_id}',response_model=RecipeModel)
 async def get_recipe(recipe_id:str):
     recipe = await recipes.find_one({"_id": ObjectId(recipe_id)})
     if recipe is None:  # Handle not found case
@@ -40,6 +47,3 @@ async def get_recipe(recipe_id:str):
 
     return recipe
 
-
-if __name__ == "__main__":
-    uvicorn.run(port=8000)
